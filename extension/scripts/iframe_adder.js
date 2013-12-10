5279424196f95401d771b5d3232bcab4e082f5c1
Zachary Yaro <zmyaro@gmail.com>
2013-12-09 21:42:38 -0600

    Make the speech recognition iframe invisible

--- extension/scripts/iframe_adder.js
+++ extension/scripts/iframe_adder.js
@@ -1,3 +1,7 @@
 var iframe = document.createElement("iframe");
 iframe.src = chrome.extension.getURL("speech_recognition.html");
+iframe.style.width = "0";
+iframe.style.height = "0";
+iframe.style.border = "0 none";
+iframe.style.display = "none";
 document.body.appendChild(iframe);
