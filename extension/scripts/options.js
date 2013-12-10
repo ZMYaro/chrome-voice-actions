c8c6c18300f1e6d58f2391af1e51edb1c0568a62
Zachary Yaro <zmyaro@gmail.com>
2013-12-09 20:10:04 -0600

    Force the omnibox media icon to be displayed on the options page

--- extension/scripts/options.js
+++ extension/scripts/options.js
@@ -12,6 +12,20 @@ window.addEventListener("load", function() {
 			setSetting(e.target.id.replace("Setting", ""), e.target.value);
 		}, false);
 	}
+	
+	// Create a speech recognition instance.
+	var speechInput = new webkitSpeechRecognition();
+	speechInput.continuous = false;
+	speechInput.interimResults = false;
+	// Add an event listener for when speech recognition starts successfully.
+	speechInput.onstart = function(e) {
+		// At this point, the omnibox media icon should be displayed.  There is
+		// no need for speech recognition to continue, so abort it.
+		e.target.abort();
+	};
+	
+	// Attempt to start speech recognition (and, as a result, display the omnibox media icon).
+	speechInput.start();
 }, false);
 
 function setSetting(setting, value) {
