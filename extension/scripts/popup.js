c502da9f97830578005ebb84837cce459c19b291
Zachary Yaro <zmyaro@gmail.com>
2014-01-14 17:43:52 -0500

    Move base URLs list to defaults.js

--- extension/scripts/popup.js
+++ extension/scripts/popup.js
@@ -10,61 +10,6 @@ var subtext;
 /** {Number} The id of the speech recognition tab */
 var speechRecTabId;
 
-/** {Object} Base URLs for the different web services */
-var baseURLs = {
-	"search":{
-		"ask":"http://www.ask.com/web?q=%s",
-		"bing":"http://www.bing.com/search?q=%s",
-		"google":"https://www.google.com/search?q=%s",
-		"yahoo":"http://search.yahoo.com/search?p=%s"
-	},
-	"images":{
-		"ask":"http://www.ask.com/pictures?q=%s",
-		"bing":"http://www.bing.com/images/search?q=%s",
-		"flickr":"http://www.flickr.com/search/?q=%s",
-		"google":"https://www.google.com/search?tbm=isch&q=%s",
-		"imgur":"http://imgur.com/gallery?q=%s",
-		"yahoo":"http://images.search.yahoo.com/search/images?p=%s"
-	},
-	"videos": {
-		"ask": "http://www.ask.com/youtube?q=%s",
-		"bing": "http://www.bing.com/videos/search?q=%s",
-		"dailymotion": "http://www.dailymotion.com/relevance/search/%s",
-		"google": "https://www.google.com/search?tbm=vid&q=%s",
-		"hulu": "http://www.hulu.com/search?q=%s",
-		"metacafe": "http://www.metacafe.com/topics/%s",
-		"netflix": "http://dvd.netflix.com/Search?v1=%s",
-		"twitch": "http://www.twitch.tv/search?query=%s",
-		"vimeo": "http://vimeo.com/search?q=%s",
-		"youtube": "https://www.youtube.com/results?search_query=%s"
-	},
-	"map":{
-		"google":"https://maps.google.com/maps?q=%s",
-		"bing":"http://www.bing.com/maps/?q=%s",
-		"yahoo":"http://maps.yahoo.com/#q=%s"
-	},
-	"directions":{
-		"google":"http://maps.google.com/maps?daddr=%s"
-	},
-	"web":{
-		"google":"https://www.google.com/search?btnI=745&q=%s"
-	},
-	"music":{
-		"amazon":"https://www.amazon.com/gp/dmusic/mp3/player#searchSongs/searchTerm=%s",
-		"google":"https://play.google.com/music/listen?u=0#%s_sr",
-		"grooveshark":"http://grooveshark.com/#!/search?q=%s",
-		"lastfm":"http://www.last.fm/search?q=%s",
-		"pandora":"http://www.pandora.com/search/%s",
-		"soundcloud": "https://soundcloud.com/search?q=%s",
-		"spotify": "https://play.spotify.com/search/%s",
-		"youtube":"http://www.youtube.com/results?search_query=%s"
-	},
-	"calc":{
-		"google":"https://www.google.com/search?q=%s",
-		"wolframalpha":"http://www.wolframalpha.com/input/?i=%s"
-	}
-}
-
 window.addEventListener("load", function() {
 	chrome.storage.sync.get({
 		actionDelayTime: defaultSettings.actionDelayTime
