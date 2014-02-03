6611ec319a0cd7d6201c657b4d0170786151d8dc
Zachary Yaro <zmyaro@gmail.com>
2014-02-02 21:42:01 -0500

    Merge in latest changes to CRX options page boilerplate

--- extension/scripts/options.js
+++ extension/scripts/options.js
@@ -2,10 +2,6 @@ window.addEventListener("load", function() {
 	// Attempt to copy settings from localStorage to synced storage.
 	copySettings();
 	
-	// Load OS styles and set up chrome:// links when the page loads.
-	loadOSStyles();
-	setUpChromeLinks();
-	
 	// Display the extension version number.
 	document.getElementById("versionNumber").innerText =
 		"Version " +
@@ -76,48 +72,4 @@ function setSetting(setting, value) {
 			alert("Something went wrong: " + chrome.runtime.lastError);
 		}
 	});
-}
-
-/**
- * Load the stylesheet that will pick the correct font for the user's OS
- */
-function loadOSStyles() {
-	var osStyle = document.createElement('link');
-	osStyle.rel = 'stylesheet';
-	osStyle.type = 'text/css';
-	if(navigator.userAgent.indexOf('Windows') !== -1) {
-		osStyle.href = 'styles/options-win.css';
-	} else if(navigator.userAgent.indexOf('Macintosh') !== -1) {
-		osStyle.href = 'styles/options-mac.css';
-	} else if(navigator.userAgent.indexOf('CrOS') !== -1) {
-		osStyle.href = 'styles/options-cros.css';
-		// Change the “Chrome” label to “Chrome OS” on CrOS.
-		document.querySelector('.sideBar h1').innerText = 'Chrome OS';
-	} else {
-		osStyle.href = 'styles/options-linux.css';
-	}
-	document.head.appendChild(osStyle);
-}
-/**
- * Change any chrome:// link to use the goToPage function
- */
-function setUpChromeLinks() {
-	// Get the list of <a>s.
-	var links = document.getElementsByTagName('a');
-	// For each link,
-	for(var i = 0; i < links.length; i++) {
-		// if the URL begins with �chrome://�,
-		if(links[i].href.indexOf('chrome://') === 0) {
-			// tell it to goToPage onclick.
-			links[i].onclick = goToPage;
-		}
-	}
-}
-/**
- * Use chrome.tabs.update to open a link Chrome will not open normally
- */
-function goToPage(e) {
-	// Prevent the browser from following the link.
-	e.preventDefault();
-	chrome.tabs.update({ url: e.target.href });
 }
