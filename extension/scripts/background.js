0144e40a048fb0f2f260392223c84b9e4dc0bdc5
Zachary Yaro <zmyaro@zmyaro.com>
2023-05-27 17:50:14 -0400

    Implement showing update page on extension update if not shown for the new version

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,13 +1,35 @@
 chrome.runtime.onInstalled.addListener(function (details) {
-	// Only show the page on first install.
 	if (details.reason === "install") {
+		// On first install, open the permission prompt page.
 		chrome.tabs.create({ url: chrome.extension.getURL("permission.html") });
+	} else if (details.reason === "update") {
+		showUpdatePageIfNew(details);
 	}
+	
 	// Attempt to copy settings from localStorage to synced storage.
 	copySettings();
 	// Set the icon color in case the user customized it on a synced account (or the extension was refreshed).
 	getSetting("toolbarIcon").then(setToolbarIcon);
 });
+
+/**
+ * Show the update page if it hasn't been shown for this version.
+ * @returns {Promise}
+ */
+async function showUpdatePageIfNew(installDetails) {
+	var currentVersion = parseFloat(chrome.runtime.getManifest().version),
+		previousVersion = parseFloat(installDetails.previousVersion),
+		lastUpdateShown = await getSetting("lastUpdateShown", 0.1, "local");
+	
+	if (previousVersion >= currentVersion || lastUpdateShown >= currentVersion) {
+		// If the update isn't a new version, don't show the update page.
+		return;
+	}
+	
+	// Open the update page.
+	chrome.tabs.create({ url: chrome.extension.getURL("update.html") });
+	chrome.storage.local.set({ lastUpdateShown: currentVersion });
+}
 
 // Attempt to copy settings from localStorage to synced storage.
 chrome.runtime.onStartup.addListener(copySettings);
